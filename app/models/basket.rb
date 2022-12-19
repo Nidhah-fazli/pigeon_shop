c1cce623f286ecf5744d5f804a41e1bbd9560f11
Nidhah-fazli <nidhahzeus@gmail.com>
2022-12-19 15:01:55 +0300

    Task-3: Total calculation and styling

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,4 +1,12 @@
 class Basket < ApplicationRecord
   has_many :items
   has_many :products, through: :items
+
+  def sub_total
+    sum = 0
+    self.items.each do |item|
+      sum+= item.total_price
+    end
+    sum
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :basket
   belongs_to :product
+
+  def total_price
+    self.quantity * self.product.price
+  end
 end
