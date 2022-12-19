01538e60fac7faa159dce4ece4a230fe1019a078
Nidhah-fazli <nidhahzeus@gmail.com>
2022-12-19 14:04:51 +0300

    Access of current basket session

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,16 +28,7 @@ class ItemsController < ApplicationController
     end
 
     def current_basket
-      if session[:basket_id]
-        basket = Basket.find_by(:id => session[:basket_id])
-        if basket.present?
-          @current_basket = basket
-        else
-          session[:basket_id] = nil
-        end
-      end
-
-      if session[:basket_id] == nil
+      if @current_basket == nil
         @current_basket = Basket.create
         session[:basket_id] = @current_basket.id
       end
